f337e82113d6f76ccff10e8286ad2f9430507ddb
Sridhar-venkata <bandasridhar2002@gmail.com>
2024-02-12 17:20:45 +0530

    code

---
+++ src/main/java/com/javatechie/Controller/Simple.java
@@ -0,0 +1,14 @@
+package com.javatechie.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Simple {
+
+    @GetMapping("/wish")
+    public String wish()
+    {
+        return "Happy birthday to you...";
+    }
+}
